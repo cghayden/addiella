7b8a829454d1b194bdc60695e241767514446f27
Corey Hayden <cghayden@gmail.com>
2024-08-16 12:42:20 -0400

    trying local updates with CI/CD

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const http = require("http");
+const http = require('http')
 
-http.createServer( function (res, res) {
-  res.write("handrolling my own server!");
-res.end();
+http
+  .createServer(function (res, res) {
+    res.write('handrolling my own server with local updates!')
+    res.end()
+  })
+  .listen(3000)
 
-}
-).listen(3000);
-
-console.log('server listening on port 3000');
+console.log('server listening on port 3000')
 console.log('yahoooooo!')
