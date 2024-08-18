615a5ab7d478781c65435751a26d2ee7e5d19349
Corey Hayden <cghayden@gmail.com>
2024-08-18 11:45:38 -0400

    finished db as in example.  db not closing properly.

--- index-ws.js
+++ index-ws.js
@@ -1,5 +1,4 @@
 const express = require('express')
-
 const server = require('http').createServer()
 const app = express()
 
@@ -18,6 +17,20 @@ server.listen(3000, () => {
 const WebSocketServer = require('ws').Server
 const wss = new WebSocketServer({ server: server }) // this is the server we created above - attach the websocket to our express server
 
+// handle shutdown gracefully ... use a process listener to catch any SIGINT command (ctrl-c) to shutdown the server gracefully and make sure we close the db and log what we want (from our shutdownDB function)
+process.on('SIGINT', () => {
+  console.log('sigint')
+  // must close down each websocket connection or server won't close
+  wss.clients.forEach(function closeEach(client) {
+    client.close()
+  })
+  server.close(() => {
+    shutdownDB()
+  })
+})
+
+// if stuck in a runnng process, 'pkill node'
+
 //  websocket states - open, closed, erroreduse listeners to handle these states
 wss.on('connection', function connection(ws) {
   // log when someone connects
@@ -26,18 +39,20 @@ wss.on('connection', function connection(ws) {
     `${new Date().toISOString()} :: client connected... number of clients: ${numClients}`
   )
 
-  // broadcast will sned a command to everyone connected
+  // broadcast will send a command to everyone connected
   wss.broadcast(`Current visitors: ${numClients}`)
 
   if (ws.readyState === ws.OPEN) {
     ws.send('welcome to the websocket server!!')
   }
+
+  // insert into db everytime someone connects
+  db.run(`INSERT INTO visitors (count, time)
+          VALUES (${numClients}, datetime('now'))`)
+
   ws.on('close', function close() {
-    const numClients = wss.clients.size
     wss.broadcast(`Current visitors: ${numClients}`)
-    console.log(
-      `${new Date().toISOString()} :: client disconnected... number of clients: ${numClients}`
-    )
+    console.log(`client disconnected... number of clients: ${numClients}`)
   })
 })
 
@@ -46,4 +61,40 @@ wss.broadcast = function broadcast(data) {
   wss.clients.forEach(function each(client) {
     client.send(data)
   })
+}
+
+/************* END WEBSOCKETS *************/
+
+/************* BEGIN DATABASE *************/
+
+const sqlite = require('sqlite3')
+
+// can write to a file or to memory
+// const db = new sqlite.Database('./myDb.db',)
+
+// in this case, we're writing to memory, though DB WILL BE WIPED WHEN WE RESTART THE SERVER
+const db = new sqlite.Database(':memory:')
+
+// set tables everytime the server starts
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE visitors (
+      count INTEGER, 
+      time TEXT
+      )
+    `)
+})
+
+function getCounts() {
+  db.each('SELECT * FROM visitors', (err, row) => {
+    console.log(row)
+  })
+}
+
+// must close db connection when server is closed
+function shutdownDB() {
+  console.log('Shutting down db')
+
+  getCounts()
+  db.close()
 }
